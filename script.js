3d4bc88985d19ede4967830e6f4110544a9e14b7
vasoyasharan <sharanvasoya@gmail.com>
2024-07-09 17:07:33 +0530

    Additional changes

--- script.js
+++ script.js
@@ -12,14 +12,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
 
-
     /*
         Indexes within the board
         [0] [1] [2]
         [3] [4] [5]
         [6] [7] [8]
     */
-
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
